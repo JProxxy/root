e714fb1fedd0c0471fc60743e6f453dcbf77fcef
Jhon Robert <nanimonanimonanimo@gmail.com>
2025-03-04 18:45:20 +0800

    2k25:03/04/2025

--- index.php
+++ index.php
@@ -7,21 +7,26 @@ $routes = [
     hash('sha256', 'dashboard') => 'templates/dashboard.php',
 ];
 
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php?page=" . array_search('templates/login.php', $routes));
-    exit();
-}
-
-// If no specific page is requested, redirect to the dashboard
-if (!isset($_GET['page'])) {
-    header("Location: index.php?page=" . array_search('templates/dashboard.php', $routes));
-    exit();
-}
-
-// Get the requested page and serve the correct file
+// Get the requested page from URL, or default to an empty string
 $page = $_GET['page'] ?? '';
 
+// If user is not logged in, redirect to the login page only if not already there
+if (!isset($_SESSION['user_id'])) {
+    $loginHash = array_search('templates/login.php', $routes);
+    if ($page !== $loginHash) {
+        header("Location: index.php?page=" . $loginHash);
+        exit();
+    }
+} else {
+    // If logged in, redirect to the dashboard only if not already there
+    $dashboardHash = array_search('templates/dashboard.php', $routes);
+    if ($page === '' || $page !== $dashboardHash) {
+        header("Location: index.php?page=" . $dashboardHash);
+        exit();
+    }
+}
+
+// Serve the requested page if it exists
 if (isset($routes[$page])) {
     include $routes[$page];
 } else {
