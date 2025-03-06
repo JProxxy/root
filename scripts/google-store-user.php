1da9664dc69e7ab3cba0c4181594c0156ff66a91
Jhon Robert <nanimonanimonanimo@gmail.com>
2025-03-07 04:20:48 +0800

    2k25:03/07/2025

--- scripts/google-store-user.php
+++ scripts/google-store-user.php
@@ -3,6 +3,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: " . (isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*'));
+header("Access-Control-Allow-Credentials: true");
+header("Vary: Origin");
+
 require_once '../app/config/connection.php';
 
 try {
