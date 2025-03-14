0bba1e1085d3401f5e03e40b420078163dedfd41
Jhon Robert <nanimonanimonanimo@gmail.com>
2025-03-15 01:31:17 +0800

    2k25:03/15

--- scripts/check-email.php
+++ scripts/check-email.php
@@ -40,7 +40,7 @@ try {
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
             $mail->Username = 'jproxxyv1@gmail.com';  
-            $mail->Password = 'ohpe beke ayed wtfn'; // Use an App Password
+            $mail->Password = 'cohw zplj ztag sifm'; // Use an App Password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port = 587;
 
