e382146047ab4d74f9eef7719f20b91ff8c6d7d8
Jhon Robert <nanimonanimonanimo@gmail.com>
2025-04-21 23:33:28 +0800

    04/20/25 7pm

--- scripts/notifyMailer.php
+++ scripts/notifyMailer.php
@@ -81,48 +81,43 @@ try {
     <hr>
 
     
-    <?php
-    echo <<<HTML
+    <!-- Footer Start -->
     <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#0D2153;color:#fff;padding:40px 20px;font-family:Arial,sans-serif;font-size:12px;line-height:1.4;">
       <tr>
         <td align="center">
-          <img src="https://yourdomain.com/assets/logo.png" alt="Your Logo" width="80" style="display:block;margin:0 auto 10px;">
-          <p><strong>Your Company</strong><br>Your Tagline</p>
+          <!-- Logo and Tagline -->
+          <img src="https://rivanit.com/assets/logo-DaYZ0U1G.png" alt="Rivan IoT Logo" width="80" style="display:block;margin:0 auto 10px;">
+          <p style="margin:0 0 20px;"><strong>RivanCyber Training Institute</strong><br>
+          Rivan IoT Notification System</p>
         </td>
       </tr>
       <tr>
         <td align="center" style="padding-bottom:20px;">
-          <!-- Social Media Links -->
-          <a href="https://www.facebook.com/YourPage" style="margin:0 5px;text-decoration:none;">
-            <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" width="24" style="display:inline-block;">
-          </a>
-          <a href="https://www.instagram.com/YourProfile" style="margin:0 5px;text-decoration:none;">
-            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" width="24" style="display:inline-block;">
-          </a>
-          <a href="https://twitter.com/YourHandle" style="margin:0 5px;text-decoration:none;">
-            <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" width="24" style="display:inline-block;">
-          </a>
+          <!-- Social Links -->
+          <a href="https://www.facebook.com/RivanInstitute" style="margin:0 5px;text-decoration:none;"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" width="24" style="display:inline-block;"></a>
+          <a href="https://m.me/RivanInstitute" style="margin:0 5px;text-decoration:none;"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111728.png" alt="Messenger" width="24" style="display:inline-block;"></a>
+          <a href="https://www.instagram.com/rivancyberinstitute" style="margin:0 5px;text-decoration:none;"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" width="24" style="display:inline-block;"></a>
         </td>
       </tr>
       <tr>
         <td style="padding-bottom:20px;text-align:center;">
-          <p style="margin:5px 0;">Your Address</p>
-          <p style="margin:5px 0;"><a href="mailto:youremail@domain.com" style="color:#fff;text-decoration:underline;">youremail@domain.com</a></p>
-          <p style="margin:5px 0;"><a href="tel:+1234567890" style="color:#fff;text-decoration:underline;">+1 234-567-890</a></p>
-          <p style="margin:5px 0;">Mon–Fri 9:00 AM – 5:00 PM</p>
+          <!-- Contact Info -->
+          <p style="margin:5px 0;">Rivan Building, 18d Mola, Makati, 1200 Metro Manila</p>
+          <p style="margin:5px 0;"><a href="mailto:teamrivan@rcvi.org" style="color:#fff;text-decoration:underline;">teamrivan@rcvi.org</a></p>
+          <p style="margin:5px 0;"><a href="tel:+639493760000" style="color:#fff;text-decoration:underline;">+63 949-376-0000</a></p>
+          <p style="margin:5px 0;"><a href="tel:+63284252848" style="color:#fff;text-decoration:underline;">+63 2-8425-2848</a></p>
+          <p style="margin:5px 0;">Mon–Fri 9:00 AM – 5:00 PM</p>
         </td>
       </tr>
       <tr>
         <td align="center" style="border-top:1px solid #444;padding-top:10px;font-size:10px;color:#ccc;">
-          © 2025 Your Company. All rights reserved.
+          © 2025 Rivan IoT. All rights reserved.
         </td>
       </tr>
     </table>
-    HTML;
-    ?>
-    
     <!-- Footer End -->
     
+    EOT;
 
     // Send the email
     $mail->send();
